a86974a1c9bce5d77749d5673e47292b59810f61
Patrick Kelleter <patrick.kelleter@netrapid.degit>
2016-03-10 23:01:37 +0100

    - initial init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # angular2-decorator-concurrent
-An Angular2 decorator function to run a function in a WebWorker
+
+An Angular2 Decorator to automate running arbitrary functions in an external thread via WebWorker
+
+Example:
+
+export class ExampleClass {
+
+	private factor = 5;
+
+	constructor() {}
+
+	@nxConcurrent
+	public hardFunction(x:number):Promise<number> {
+		let it = 0;
+		const s = new Date().getTime();
+		while (new Date().getTime() - s < 4000) {
+			it++;
+		}
+		const promise = new Promise(
+			(resolve, reject) => {
+				(x % 2 === 0 ? resolve : reject)(this.factor * x);
+			}
+		);
+		return promise;
+	}
+
+	public handleWebWorkerButtonClick(param) {
+		this.hardFunction(param).then(
+			(data) => {
+				console.log('Success! ', data);
+			},
+			(error) => {
+				console.error ('Error! ', error);
+			}
+		)
+	}
+
+}
+
+
+Restrictions:
+
+- Since the decorated function will have to return the calculated data as a Promise, since it uses
+  a WebWorker to generate the data, the decorated function must return a Promise beforehand.
+  Hence only function with a return type of "Promise<any>"" will be decoratable
+- Due to technical restrictions the decorated function will only be able to READ variables from its
+  context .
+  (e.g. "this.factor" in the given example is read-only)
+- Other functions in the given context will not be accessible at all - so make sure you
+  do not call any other functions inside your decorated function.
+  (e.g. "handleWebWorkerButtonClick" in the given example would NOT be accessible)
---
+++ concurrent.ts
@@ -0,0 +1,73 @@
+interface IWebWorkerMessageEvent<T> extends MessageEvent {
+	data: {
+		data: T,
+		rejected: boolean
+	}
+}
+
+export function nxConcurrent<T> (target:Object,
+								 propertyKey:string,
+								 descriptor:TypedPropertyDescriptor<(...args:any[]) => Promise<T>>) {
+
+	// remember original function for later invokation as a normalized string (no linebreaks)
+	const _originalFunctionAsString = descriptor.value.toString().replace(/(\r\n|\n|\r)/gm, "");
+
+	// wrap original function by web worker
+	descriptor.value = function (...args:any[]) {
+		const promise = new Promise(
+			(resolve, reject) => {
+				// create object url from function
+				const objectUrl = URL.createObjectURL(
+					new Blob([`
+							self.addEventListener(
+								'message',
+								function (evt) {
+									var eventData = JSON.parse(evt.data),
+										scope = eventData.scope,
+										params = eventData.params,
+										fnGenerator = new Function("return ${_originalFunctionAsString}"),
+										fn = fnGenerator(),
+										resultingPromise = fn.apply(scope, params);
+									resultingPromise.then (
+										function (result) {
+											self.postMessage({
+												data: result,
+												rejected: false
+											});
+										},
+										function (error) {
+											self.postMessage({
+												data: error,
+												rejected: true
+											});
+										}
+									);
+								},
+								false
+							);
+						`], {type: 'application/javascript'})
+				);
+
+				// create web worker and release resources
+				const worker = new Worker(objectUrl);
+				URL.revokeObjectURL(objectUrl);
+
+				// add listeners to webworker
+				worker.onmessage = function (evt:IWebWorkerMessageEvent<T>) {
+					(evt.data.rejected ? reject : resolve)(evt.data.data);
+				};
+				worker.onerror = reject;
+				// start webworker
+				worker.postMessage(
+					JSON.stringify({
+						scope: this,
+						params: args
+					})
+				);
+			}
+		);
+		return promise;
+	};
+	return descriptor;
+
+}
